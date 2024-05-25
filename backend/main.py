311460761034ed12b4a5b3005dbb628a7fb75922
nobaseosusang <talisman1131@unist.ac.kr>
2024-05-25 13:30:40 +0900

    Model change

--- backend/main.py
+++ backend/main.py
@@ -1,81 +1,59 @@
-from fastapi import FastAPI, HTTPException, File, UploadFile, Form
+from fastapi import FastAPI, HTTPException, File, UploadFile
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from typing import Optional
 import io
 from PIL import Image
 import services as _service
-import json
 
-# 데이터 모델 정의
+
 class ImageCreate(BaseModel):
     prompt: str
-    seed: Optional[int] = 42
-    num_inference_steps: int = 10
+    seed: Optional[int] = 0
+    num_inference_steps: int = 30
     guidance_scale: float = 7.5
+    model_name: Optional[str] = "CompVis/stable-diffusion-v1-4"
+
+    class Config:
+        protected_namespaces = ()
 
 class Img2ImgCreate(BaseModel):
     prompt: str
-    seed: Optional[int] = 42
-    num_inference_steps: int = 10
+    seed: Optional[int] = 0
+    num_inference_steps: int = 30
     guidance_scale: float = 7.5
+    model_name: Optional[str] = "CompVis/stable-diffusion-v1-4"
 
-# FastAPI 애플리케이션 인스턴스 생성
+    class Config:
+        protected_namespaces = ()
+
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Stable Diffusers API에 오신 것을 환영합니다"}
 
-# 텍스트 프롬프트를 기반으로 이미지를 생성하기 위한 엔드포인트
 @app.post("/api/generate/")
 async def generate_image(params: ImageCreate):
     try:
-        # 이미지 생성 서비스 호출
         image = await _service.generate_image(params)
-
-        # 메모리 스트림에 이미지를 저장
         memory_stream = io.BytesIO()
         image.save(memory_stream, format="PNG")
-        memory_stream.seek(0)  # 스트림의 시작 위치로 이동
-
-        # 생성된 이미지를 스트리밍 응답으로 반환
+        memory_stream.seek(0)
         return StreamingResponse(memory_stream, media_type="image/png")
     except Exception as e:
-        # 예외 발생 시 클라이언트에게 오류 메시지 반환
         raise HTTPException(status_code=500, detail=str(e))
 
-# 이미지를 기반으로 이미지를 생성하기 위한 엔드포인트
 @app.post("/api/img2img/")
-async def img2img(
-    prompt: str = Form(...),
-    seed: Optional[int] = Form(42),
-    num_inference_steps: int = Form(10),
-    guidance_scale: float = Form(7.5),
-    file: UploadFile = File(...)
-):
+async def img2img(params: Img2ImgCreate, file: UploadFile = File(...)):
     try:
-        # JSON 데이터를 Img2ImgCreate 모델로 변환
-        params = Img2ImgCreate(
-            prompt=prompt,
-            seed=seed,
-            num_inference_steps=num_inference_steps,
-            guidance_scale=guidance_scale
-        )
-
-        # 업로드된 이미지를 열기
         input_image = Image.open(file.file).convert("RGB")
-
-        # 이미지 생성 서비스 호출
         image = await _service.img2img(params, input_image)
-
-        # 메모리 스트림에 이미지를 저장
         memory_stream = io.BytesIO()
         image.save(memory_stream, format="PNG")
-        memory_stream.seek(0)  # 스트림의 시작 위치로 이동
-
-        # 생성된 이미지를 스트리밍 응답으로 반환
+        memory_stream.seek(0)
         return StreamingResponse(memory_stream, media_type="image/png")
     except Exception as e:
-        # 예외 발생 시 클라이언트에게 오류 메시지 반환
         raise HTTPException(status_code=500, detail=str(e))
+
--- backend/services.py
+++ backend/services.py
@@ -2,64 +2,66 @@ from pathlib import Path
 import schemas as _schemas
 import torch
 from diffusers import StableDiffusionPipeline, StableDiffusionImg2ImgPipeline
+from safetensors.torch import load_file
 from PIL import Image
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# HuggingFace 토큰 가져오기
-HF_TOKEN = os.getenv('HF_TOKEN')
 
-# 텍스트 기반 이미지 생성 파이프라인 생성
-text_pipe = StableDiffusionPipeline.from_pretrained(
-    "CompVis/stable-diffusion-v1-4", 
-    revision="fp16", 
-    torch_dtype=torch.float16,
-    use_auth_token=HF_TOKEN
-)
-
-# 이미지 기반 이미지 생성 파이프라인 생성
-img2img_pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
-    "CompVis/stable-diffusion-v1-4", 
-    revision="fp16", 
-    torch_dtype=torch.float16,
-    use_auth_token=HF_TOKEN
-)
-
-# 사용할 장치 설정
 if torch.backends.mps.is_available():
     device = "mps"
 else: 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-text_pipe.to(device)
-img2img_pipe.to(device)
 
-# 텍스트 기반 이미지 생성 함수
-async def generate_image(imgPrompt: _schemas.ImageCreate) -> Image: 
-    generator = None if imgPrompt.seed is None else torch.Generator(device=device).manual_seed(int(imgPrompt.seed))
+def load_safetensors_model(model_path):
+    state_dict = load_file(model_path)
+    return state_dict
 
-    image: Image = text_pipe(
+
+async def generate_image(imgPrompt: _schemas.ImageCreate) -> Image:
+    model_name_or_path = imgPrompt.model_name
+    if model_name_or_path.endswith('.safetensors'):
+        state_dict = load_safetensors_model(model_name_or_path)
+        pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", state_dict=state_dict)
+    else:
+        pipe = StableDiffusionPipeline.from_pretrained(
+            model_name_or_path,
+            torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32
+        )
+    pipe.to(device)
+    
+    generator = None if imgPrompt.seed == 0 else torch.Generator(device=device).manual_seed(int(imgPrompt.seed))
+    image: Image = pipe(
         imgPrompt.prompt,
         guidance_scale=imgPrompt.guidance_scale, 
         num_inference_steps=imgPrompt.num_inference_steps, 
         generator=generator
     ).images[0]
-    
     return image
 
-# 이미지 기반 이미지 생성 함수
-async def img2img(imgPrompt: _schemas.Img2ImgCreate, init_image: Image) -> Image:
-    generator = None if imgPrompt.seed is None else torch.Generator(device=device).manual_seed(int(imgPrompt.seed))
 
-    image: Image = img2img_pipe(
+async def img2img(imgPrompt: _schemas.Img2ImgCreate, init_image: Image) -> Image:
+    model_name_or_path = imgPrompt.model_name
+    if model_name_or_path.endswith('.safetensors'):
+        state_dict = load_safetensors_model(model_name_or_path)
+        pipe = StableDiffusionImg2ImgPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", state_dict=state_dict)
+    else:
+        pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
+            model_name_or_path,
+            torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32
+        )
+    pipe.to(device)
+    
+    generator = None if imgPrompt.seed == 0 else torch.Generator(device=device).manual_seed(int(imgPrompt.seed))
+    image: Image = pipe(
         prompt=imgPrompt.prompt,
-        image=init_image,  # 이미지 인자를 추가
+        image=init_image,
         strength=0.75,  # Strength 파라미터 조절 가능
         guidance_scale=imgPrompt.guidance_scale,
         num_inference_steps=imgPrompt.num_inference_steps,
         generator=generator
     ).images[0]
-
     return image
